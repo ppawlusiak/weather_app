1f2f2931c409d9467c596165a049c1dea8204554
Piotr Pawlusiak <ppawlusiak@gmail.com>
2024-08-28 17:51:43 +0200

    Create basic layout

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -23,6 +23,24 @@ class WeatherScreen extends StatelessWidget {
           )
         ],
       ),
+      body: Column(
+        children: [
+          // main card
+          const Placeholder(
+            fallbackHeight: 250,
+          ),
+          const SizedBox(height: 20),
+          // forecast cards
+          const Placeholder(
+            fallbackHeight: 150,
+          ),
+          const SizedBox(height: 20),
+          // additional information
+          const Placeholder(
+            fallbackHeight: 150,
+          )
+        ],
+      ),
     );
   }
 }
