933ecacecbfa75932e9873bb3d9cf7fff5d73d72
Piotr Pawlusiak <ppawlusiak@gmail.com>
2024-08-28 18:33:37 +0200

    Create main card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -23,23 +25,74 @@ class WeatherScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        children: [
-          // main card
-          const Placeholder(
-            fallbackHeight: 250,
-          ),
-          const SizedBox(height: 20),
-          // forecast cards
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-          const SizedBox(height: 20),
-          // additional information
-          const Placeholder(
-            fallbackHeight: 150,
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shadowColor: Colors.black87,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 10,
+                      sigmaY: 10,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300 °F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // forecast cards
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // additional information
+            const Placeholder(
+              fallbackHeight: 150,
+            )
+          ],
+        ),
       ),
     );
   }
