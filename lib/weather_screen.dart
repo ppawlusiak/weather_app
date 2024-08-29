747e58e6e2146ef78cdbc8d7d4998d9c505d4d24
Piotr Pawlusiak <ppawlusiak@gmail.com>
2024-08-29 22:06:28 +0200

    Create forecast card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -28,6 +28,7 @@ class WeatherScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // main card
             SizedBox(
@@ -81,8 +82,27 @@ class WeatherScreen extends StatelessWidget {
               height: 20,
             ),
             // forecast cards
-            const Placeholder(
-              fallbackHeight: 150,
+            const Text(
+              'Weather Forecast',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HurlyForecastItem(),
+                  HurlyForecastItem(),
+                  HurlyForecastItem(),
+                  HurlyForecastItem(),
+                  HurlyForecastItem(),
+                ],
+              ),
             ),
             const SizedBox(
               height: 20,
@@ -90,6 +110,51 @@ class WeatherScreen extends StatelessWidget {
             // additional information
             const Placeholder(
               fallbackHeight: 150,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HurlyForecastItem extends StatelessWidget {
+  const HurlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Column(
+          children: [
+            Text(
+              '03:00',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Icon(
+              Icons.cloud,
+              size: 32,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              '300.12 °F',
+              style: TextStyle(
+                fontSize: 14,
+              ),
             )
           ],
         ),
